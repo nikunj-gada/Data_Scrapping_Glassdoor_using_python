97149e79a5217cb53c46fa35916a4bb08c2dfcfe
NIKUNJ <64151417+nikunj1895@users.noreply.github.com>
2021-05-24 23:44:39 +0530

    [nikunj] Added comments and updated a variable
    [nikunj] Added comments and updated a variable

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Created on Thu May 20 16:48:31 2021
 @author: Nikunj Gada - https://www.linkedin.com/in/nikunj-gada
 """
 #Get necessary imports
-import get_jobs as jobs
+import data_collection as dc
 import logging
 import logging.config
 import os
@@ -26,10 +26,11 @@ sleepTime = int(0)
 #number of records you want to fetch
 numOfRecords = int(1600)
 
+#Url to fetch all the data scientist jobs in India. Note : use .com and not .in
 url = 'https://www.glassdoor.com/Job/india-data-scientist-jobs-SRCH_IL.0,5_IN115_KO6,20.htm?minSalary=52000&maxSalary=1008000&includeNoSalaryJobs=false'
 
 logger.info('Fetching data')
-df = jobs.get_jobs(url, numOfRecords, False, driver, sleepTime)
+df = dc.get_jobs(url, numOfRecords, False, driver, sleepTime)
 
 logger.info('data received. Creating csv file as gs_data.csv')
 df.to_csv("gs_data.csv",index=False,header=True)
